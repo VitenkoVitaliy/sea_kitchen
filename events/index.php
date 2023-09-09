2083086a5e14a62841b7a354850549f59efca923
yg <yuliagruzintsewa@yandex.ru>
2023-09-09 17:17:45 +0300

    initial commit

---
+++ events/index.php
@@ -0,0 +1,121 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Мероприятия");
+?>
+
+    <!-- Start Align Area -->
+    <div class="whole-wrap">
+        <div class="container box_1170">
+            <div class="section-top-border">
+                <h3 class="mb-30">Расписание мероприятий</h3>
+                <div class="progress-table-wrap">
+                    <div class="progress-table">
+                        <div class="table-head">
+                            <div class="date">Дата</div>
+                            <div class="title">Название</div>
+                            <div class="agenda">Программа</div>
+                            <div class="detail">Регистрация</div>
+                        </div>
+                        <div class="table-row">
+                            <div class="date">01.07.2022</div>
+                            <div class="title">Греческий вечер</div>
+                            <div class="agenda">
+                                Встреча гостей. 
+                                Приветственный напиток.
+                                Концерт живой музыки
+                            </div>
+                            <div class="detail">
+                                    <a href="events_detail.html" class="genric-btn success medium">Подробнее</a>
+                            </div>
+                        </div>
+                        <div class="table-row">
+                            <div class="date">02.08.2022</div>
+                            <div class="title">Греческий вечер</div>
+                            <div class="agenda">
+                                Встреча гостей. 
+                                Приветственный напиток.
+                                Концерт живой музыки
+                            </div>
+                            <div class="detail">
+                                <a href="events_detail.html" class="genric-btn success medium">Подробнее</a>
+                            </div>
+                        </div>
+                        <div class="table-row">
+                            <div class="date">03.09.2022</div>
+                            <div class="title">Греческий вечер</div>
+                            <div class="agenda">
+                                Встреча гостей. 
+                                Приветственный напиток.
+                                Концерт живой музыки
+                            </div>
+                            <div class="detail">
+                                <a href="events_detail.html" class="genric-btn success medium">Подробнее</a>
+                            </div>
+                        </div>
+                        <div class="table-row">
+                            <div class="date">01.07.2022</div>
+                            <div class="title">Греческий вечер</div>
+                            <div class="agenda">
+                                Встреча гостей. 
+                                Приветственный напиток.
+                                Концерт живой музыки
+                            </div>
+                            <div class="detail">
+                                <a href="events_detail.html" class="genric-btn success medium">Подробнее</a>
+                            </div>
+                        </div>
+                        <div class="table-row">
+                            <div class="date">01.07.2022</div>
+                            <div class="title">Греческий вечер</div>
+                            <div class="agenda">
+                                Встреча гостей. 
+                                Приветственный напиток.
+                                Концерт живой музыки
+                            </div>
+                            <div class="detail">
+                                <a href="events_detail.html" class="genric-btn success medium">Подробнее</a>
+                            </div>
+                        </div>
+                        <div class="table-row">
+                            <div class="date">01.07.2022</div>
+                            <div class="title">Греческий вечер</div>
+                            <div class="agenda">
+                                Встреча гостей. 
+                                Приветственный напиток.
+                                Концерт живой музыки
+                            </div>
+                            <div class="detail">
+                                <a href="events_detail.html" class="genric-btn success medium">Подробнее</a>
+                            </div>
+                        </div>
+                        <div class="table-row">
+                            <div class="date">01.07.2022</div>
+                            <div class="title">Греческий вечер</div>
+                            <div class="agenda">
+                                Встреча гостей. 
+                                Приветственный напиток.
+                                Концерт живой музыки
+                            </div>
+                            <div class="detail">
+                                <a href="events_detail.html" class="genric-btn success medium">Подробнее</a>
+                            </div>
+                        </div>
+                        <div class="table-row">
+                            <div class="date">01.07.2022</div>
+                            <div class="title">Греческий вечер</div>
+                            <div class="agenda">
+                                Встреча гостей. 
+                                Приветственный напиток.
+                                Концерт живой музыки
+                            </div>
+                            <div class="detail">
+                                <a href="events_detail.html" class="genric-btn success medium">Подробнее</a>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+.
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
