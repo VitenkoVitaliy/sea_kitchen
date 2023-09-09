8aa9916e025e1d11255561a650962fdcd89fc4bd
yg <yuliagruzintsewa@yandex.ru>
2023-09-10 00:47:37 +0300

    added templates inner main

---
+++ index.php
@@ -0,0 +1,240 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Ресторан Морская кухня");
+?>
+
+
+
+<!-- gallery_start -->
+    <div class="gallery_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="section_title text-center mb-75">
+                        <h3>Самое вкусное</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="single_gallery big_img">
+            <a class="popup-image" href="<?=SITE_TEMPLATE_PATH?>/img/gallery/1.png"></a>
+            <img src="<?=SITE_TEMPLATE_PATH?>/img/gallery/1.png" alt="">
+        </div>
+        <div class="single_gallery small_img">
+            <a class="popup-image" href="<?=SITE_TEMPLATE_PATH?>/img/gallery/2.png"></a>
+            <img src="<?=SITE_TEMPLATE_PATH?>/img/gallery/2.png" alt="">
+        </div>
+        <div class="single_gallery small_img">
+            <a class="popup-image" href="<?=SITE_TEMPLATE_PATH?>/img/gallery/3.png"></a>
+            <img src="<?=SITE_TEMPLATE_PATH?>/img/gallery/3.png" alt="">
+        </div>
+
+        <div class="single_gallery small_img">
+            <a class="popup-image" href="<?=SITE_TEMPLATE_PATH?>/img/gallery/4.png"></a>
+            <img src="<?=SITE_TEMPLATE_PATH?>/img/gallery/4.png" alt="">
+        </div>
+        <div class="single_gallery small_img">
+            <a class="popup-image" href="<?=SITE_TEMPLATE_PATH?>/img/gallery/5.png"></a>
+            <img src="<?=SITE_TEMPLATE_PATH?>/img/gallery/5.png" alt="">
+        </div>
+        <div class="single_gallery big_img">
+            <a class="popup-image" href="<?=SITE_TEMPLATE_PATH?>/img/gallery/6.png"></a>
+            <img src="<?=SITE_TEMPLATE_PATH?>/img/gallery/6.png" alt="">
+        </div>
+    </div>
+    
+    <!-- gallery end -->
+    
+    <!-- Мероприятия -->
+    <div class="event_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="section_title text-center mb-50">
+                        <h3>Мероприятия</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="section-top-border">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="single-defination">
+                            <h4 class="mb-20">Мероприятие 01</h4>
+                            <p>Recently, the US Federal government banned online casinos from operating in America by
+                                making it illegal to
+                                transfer money to them through any US bank or payment system. As a result of this law,
+                                most of the popular
+                                online casino networks</p>
+                                <a href="events/events_detail.php" class="genric-btn default">Подробнее</a>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="single-defination">
+                            <h4 class="mb-20">Мероприятие 02</h4>
+                            <p>Recently, the US Federal government banned online casinos from operating in America by
+                                making it illegal to
+                                transfer money to them through any US bank or payment system. As a result of this law,
+                                most of the popular
+                                online casino networks</p>
+                                <a href="events/events_detail.php" class="genric-btn default">Подробнее</a>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="single-defination">
+                            <h4 class="mb-20">Мероприятие 03</h4>
+                            <p>Recently, the US Federal government banned online casinos from operating in America by
+                                making it illegal to
+                                transfer money to them through any US bank or payment system. As a result of this law,
+                                most of the popular
+                                online casino networks</p>
+                                <a href="events/events_detail.php" class="genric-btn default">Подробнее</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- / Мероприятия -->
+    <!-- testimonial_area  -->
+    <div class="testimonial_area overlay ">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="section_title text-center mb-50">
+                        <p>Отзывы</p>
+                        <h3>Наши клиенты говорят:</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="testmonial_active owl-carousel">
+                        <div class="single_carousel">
+                            <div class="single_testmonial ">
+                                <div class="author_opinion">
+                                    <p>“Donec imperdiet congue orci consequat mattis. Donec rutrum porttitor
+                                        sollicitudin. Pellentesque id dolor tempor sapien feugiat ultrices nec sed
+                                        neque.</p>
+                                </div>
+                                <div class="testmonial_author">
+                                    <div class="thumb">
+                                        <img src="<?=SITE_TEMPLATE_PATH?>/img/testimonial/author.png" alt="">
+                                    </div>
+                                    <div class="name">
+                                        <h3>Robert Thomson</h3>
+                                        <div class="icon">
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single_carousel">
+                            <div class="single_testmonial ">
+                                <div class="author_opinion">
+                                    <p>“Donec imperdiet congue orci consequat mattis. Donec rutrum porttitor
+                                        sollicitudin. Pellentesque id dolor tempor sapien feugiat ultrices nec sed
+                                        neque.</p>
+                                </div>
+                                <div class="testmonial_author">
+                                    <div class="thumb">
+                                        <img src="<?=SITE_TEMPLATE_PATH?>/img/testimonial/author2.png" alt="">
+                                    </div>
+                                    <div class="name">
+                                        <h3>Kristiana Chouhan</h3>
+                                        <div class="icon">
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single_carousel">
+                            <div class="single_testmonial ">
+                                <div class="author_opinion">
+                                    <p>“Donec imperdiet congue orci consequat mattis. Donec rutrum porttitor
+                                        sollicitudin. Pellentesque id dolor tempor sapien feugiat ultrices nec sed
+                                        neque.</p>
+                                </div>
+                                <div class="testmonial_author">
+                                    <div class="thumb">
+                                        <img src="<?=SITE_TEMPLATE_PATH?>/img/testimonial/author2.png" alt="">
+                                    </div>
+                                    <div class="name">
+                                        <h3>Kristiana Chouhan</h3>
+                                        <div class="icon">
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single_carousel">
+                            <div class="single_testmonial ">
+                                <div class="author_opinion">
+                                    <p>“Donec imperdiet congue orci consequat mattis. Donec rutrum porttitor
+                                        sollicitudin. Pellentesque id dolor tempor sapien feugiat ultrices nec sed
+                                        neque.</p>
+                                </div>
+                                <div class="testmonial_author">
+                                    <div class="thumb">
+                                        <img src="<?=SITE_TEMPLATE_PATH?>/img/testimonial/author.png" alt="">
+                                    </div>
+                                    <div class="name">
+                                        <h3>Robert Thomson</h3>
+                                        <div class="icon">
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="single_carousel">
+                            <div class="single_testmonial ">
+                                <div class="author_opinion">
+                                    <p>“Donec imperdiet congue orci consequat mattis. Donec rutrum porttitor
+                                        sollicitudin. Pellentesque id dolor tempor sapien feugiat ultrices nec sed
+                                        neque.</p>
+                                </div>
+                                <div class="testmonial_author">
+                                    <div class="thumb">
+                                        <img src="<?=SITE_TEMPLATE_PATH?>/img/testimonial/author2.png" alt="">
+                                    </div>
+                                    <div class="name">
+                                        <h3>Kristiana Chouhan</h3>
+                                        <div class="icon">
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                            <a href="#"><i class="fa fa-star"></i> </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /testimonial_area  -->
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
